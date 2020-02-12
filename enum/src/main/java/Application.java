ee956649f2b492163ee7e6d4110bfc4aa7716829
RicoTech666 <zhoujiachenbe@gmail.com>
2020-02-12 12:13:15 +0800

    [Jiachen] feat: add classes

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,16 @@
+import module.Gender;
+import module.Student;
+
 public class Application {
-
-  public static void main(String[] args) {
-
-  }
+    public static void main(String[] args) {
+        Student stu1 = new Student("Linda", Gender.FEMALE);
+        Student stu2 = new Student("Bob", Gender.MALE);
+        Student stu3 = new Student("Solider", Gender.MALE);
+        Student stu4 = new Student("Cindy", Gender.FEMALE);
+        stu1.findFemaleAndPrint();
+        stu2.findFemaleAndPrint();
+        stu3.findFemaleAndPrint();
+        stu4.findFemaleAndPrint();
+    }
 
 }
